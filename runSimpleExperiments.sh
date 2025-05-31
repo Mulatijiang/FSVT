fcbe7c4562b2963a9c63445be720db0c55547747
Mulatijiang <20281252@bjtu.edu.cn>
2025-05-31 21:06:01 +0800

    Update configure scripts

--- runSimpleExperiments.sh
+++ runSimpleExperiments.sh
@@ -7,7 +7,7 @@ echo -e "Starting Mutation Testing for Max Spec (Correct)"
 
 mkdir experimentOutput/MaxSpecCorrect
 
-./Binaries/Dafny /compile:0 /timeLimit:1520 /trace /arith:5 /noCheating:1 /mutationTarget:maxExample.maxSpec /proofName:maxTest.maxT  /proofLocation:"$(pwd)/specs/max/lemmaMaxTestCorrect.dfy" /serverIpPortList:ipPorts.txt $(pwd)/specs/max/maxSpec.dfy &> experimentOutput/MaxSpecCorrect/maxSpecCorrect_output.txt
+./Binaries/Dafny /compile:0 /timeLimit:1520 /trace /arith:5 /noCheating:1 /mutationTarget:maxExample.maxSpec /proofName:maxTest.maxT  /proofLocation:"$(pwd)/specs/max/lemmaMaxTestCorrect.dfy" /serverIpPortList:ipPorts.txt $(pwd)/specs/max/maxSpec.dfy 2>&1 | tee experimentOutput/MaxSpecCorrect/maxSpecCorrect_output.txt
 
 mkdir experimentOutput/MaxSpecCorrect/outputLogs
 cp ./outputLogs/* experimentOutput/MaxSpecCorrect/outputLogs
@@ -21,6 +21,6 @@ echo -e "Starting Automatic Sanity Checker(ASC) Testing for Sort"
 
 mkdir experimentOutput/sortASC
 
-./Binaries/Dafny /compile:0 /timeLimit:1520 /trace /arith:5 /noCheating:1 /mutationTarget:sort.SortSpec /proofName:sort.merge_sort /mutationRootName:sort.SortSpec /proofLocation:"$(pwd)/specs/sort/sortMethod.dfy" /serverIpPortList:ipPorts.txt /checkInputAndOutputSpecified $(pwd)/specs/sort/sortMethod.dfy &> experimentOutput/sortASC/sortASC_output.txt
+./Binaries/Dafny /compile:0 /timeLimit:1520 /trace /arith:5 /noCheating:1 /mutationTarget:sort.SortSpec /proofName:sort.merge_sort /mutationRootName:sort.SortSpec /proofLocation:"$(pwd)/specs/sort/sortMethod.dfy" /serverIpPortList:ipPorts.txt /checkInputAndOutputSpecified $(pwd)/specs/sort/sortMethod.dfy 2>&1 | tee experimentOutput/sortASC/sortASC_output.txt
 
 echo -e "DONE ASC Testing for sort - output found in ./experimentOutput/sortASC/sortASC_output.txt \n"
